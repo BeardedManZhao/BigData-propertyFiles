eaee8bb1456181f66647fe8e0375136301dfde0c
LingYuZhao <liming7887@qq.com>
2023-11-24 22:56:32 +0800

    update

--- hive/本地配置/mysql配置.sql
+++ hive/本地配置/mysql配置.sql
@@ -1,16 +1,21 @@
-#apt-get update
-# apt install mysql-server
-# 启动MySQL
-# /etc/init.d/mysql start
-
-# 创建好hive的元数据库
+#apt
+-get
+update
+    # apt install mysql-server
+    # 启动MySQL
+    # /etc/init.d/mysql
+start
+    # 创建好hive的元数据库
 create
-    database if not exists hive;
-# 创建一个用户hive 指定其密码为hive1234
+database if not exists hive;
+#
+创建一个用户hive 指定其密码为hive1234
 CREATE
-    USER 'hive'@'%' IDENTIFIED BY 'Hive@12341234';
-# 赋予该用户针对 hive 库中表的所有权限
+USER 'hive'@'%' IDENTIFIED BY 'Hive@12341234';
+#
+赋予该用户针对 hive 库中表的所有权限
 grant all on hive.* to 'hive'@'%';
-# 查询用户表信息
+#
+查询用户表信息
 select user, host
 from mysql.user;
